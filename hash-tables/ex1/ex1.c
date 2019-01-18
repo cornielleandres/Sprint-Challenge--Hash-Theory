764014525abaacabf77a55a624f9f34df1329ec9
Carlos Vargas <cornielleandres@gmail.com>
2019-01-18 13:18:36 -0500

    Free answer structs to prevent memory leak.

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -49,20 +49,28 @@ int main(void)
   Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
   print_answer(answer_1);  // NULL
 
+  free(answer_1);
+
   // TEST 2
   int weights_2[] = {4, 4};
   Answer* answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
   print_answer(answer_2);  // {1, 0}
+
+  free(answer_2);
 
   // TEST 3
   int weights_3[] = {4, 6, 10, 15, 16};
   Answer* answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
   print_answer(answer_3);  // {3, 1}
 
+  free(answer_3);
+
   // TEST 4
   int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
   Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
   print_answer(answer_4);  // {6, 2}
+
+  free(answer_4);
 
   return 0;
 }
